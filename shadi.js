c7a1c2a79ad9a97c62823765fa43d665577aa867
mohojojo <balazs@virtdb.com>
2018-03-10 14:05:51 +0100

    random

--- shadi.js
+++ shadi.js
@@ -8,46 +8,89 @@ var shadi  = {
   img : "",
 };
 
+const lines = {
+  line1: {
+    speed: 50,
+    direction: -1,
+    x: 500,
+    y: 100
+  },
+  line2: {
+    speed: 50,
+    direction: -1,
+    x: 500,
+    y: 150
+  },
+  line3: {
+    speed: 50,
+    direction: -1,
+    x: 500,
+    y: 200
+  },
+  line4: {
+    speed: 50,
+    direction: 1,
+    x: -50,
+    y: 300
+  },
+  line5: {
+    speed: 50,
+    direction: 1,
+    x: -50,
+    y: 350
+  },
+  line6: {
+    speed: 50,
+    direction: 1,
+    x: -50,
+    y: 400
+  },
+}
+
 const enemies = {
-  bus: function(x, y, direction) {
-    this.x = x;
-    this.y = y;
-    this.speed = getRandomSpeed(50, 100);
+  bus: function(line) {
+    this.x = line.x;
+    this.y = line.y;
+    this.speed = line.speed;
     this.image = new Image(50, 50);
     this.image.src = 'SzaboAdam.jpg';
-    this.direction = direction;
+    this.direction = line.direction;
   },
 
-  car: function(x, y, direction) {
-    this.x = x;
-    this.y = y;
-    this.speed = getRandomSpeed(10, 75);
+  car: function(line) {
+    this.x = line.x;
+    this.y = line.y;
+    this.speed = line.speed;
     this.image = new Image(50, 50);
     this.image.src = 'SzaboAdam.jpg';
-    this.direction = direction;
+    this.direction = line.direction;
   },
 
-  motor: function(x, y, direction) {
-    this.x = x;
-    this.y = y;
-    this.speed = getRandomSpeed(50, 100);
+  motor: function(line) {
+    this.x = line.x;
+    this.y = line.y;
+    this.speed = line.speed;
     this.image = new Image(50, 50);
     this.image.src = 'SzaboAdam.jpg';
-    this.direction = direction;
+    this.direction = line.direction;
   },
 
-  TEK: function(x, y, direction) {
-    this.x = x;
-    this.y = y;
-    this.speed = getRandomSpeed(50, 100);
+  TEK: function(line) {
+    this.x = line.x;
+    this.y = line.y;
+    this.speed = line.speed;
     this.image = new Image(50, 50);
     this.image.src = 'SzaboAdam.jpg';
-    this.direction = direction;
+    this.direction = line.direction;
   }
 }
 
-function getRandomSpeed(min, max) {
-  return Math.random() * (max - min) + min;
+function getLine(line) {
+  return lines['line'+line];
+};
+
+function getRandomLine() {
+  return Math.round(Math.random() * (6 - 1) + 1);
 }
 
 function renderShadi(x, y) {
@@ -99,17 +142,33 @@ function startGame() {
 
 function generateRandomEnemies() {
 
-  // var enemyStarter = setInterval(function() {
-  //   existingEnemies.push(new enemies.car(-50, 350));
-  // }, 1000)
+  var enemyStarter = setInterval(function() {
+    var line = getRandomLine();
+    existingEnemies.push(new enemies.car(getLine(line)));
+  }, 1000)
+
+  var enemyStarter2 = setInterval(function() {
+    var line = getRandomLine();
+    existingEnemies.push(new enemies.car(getLine(line)));
+  }, 1000)
 
   var carTimeout = setInterval(function() {
     ctx.clearRect(0,0,500,500);
     existingEnemies.forEach(function(item) {
-      item.x += item.speed;
+      item.x += (item.speed * item.direction);
       renderEnemy(item);
       renderShadi(shadi.x, shadi.y);
     });
 
   }, 100);
 }
+
+function explosion(lifes, pos_x, pos_y) {
+  console.log("lofasz")
+ 
+  document.getElementById("explosion_container").style ="position:absolute;" + "top: " + pos_x + "px;"  + "left: " + pos_y + "px;";
+  document.getElementById("explosion_img").src="Game/explosion1.gif"
+  
+  console.log(current_visual_source);
+  
+};
